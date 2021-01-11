b7d5401e66b4c31c60fa4e7c6707d17d608610d5
Louise Shen <CinemaMediaGroup@iCloud.com>
2021-01-11 22:25:42 +0800

    feat(projetc): fINALLY WORKS!!! THOUGH it really sucks

--- WebP.Swift/main.swift
+++ WebP.Swift/main.swift
@@ -7,5 +7,20 @@
 
 import Foundation
 
-print("Hello, World!")
+func makeCString(_ str: String) -> UnsafeMutablePointer<Int8> {
+    let count = str.utf8.count + 1
+    let result = UnsafeMutablePointer<Int8>.allocate(capacity: count)
+    str.withCString { (baseAddress) in
+        result.initialize(from: baseAddress, count: count)
+    }
+    return result
+}
 
+cwebp(5,
+      makeCString("-lossless"),
+      makeCString("/Users/louisshen/Desktop/bp.png"),
+      makeCString("-o"),
+      makeCString("/Users/louisshen/Desktop/bp.webp"),
+      makeCString("-v"),
+      makeCString(""),
+      makeCString(""))
