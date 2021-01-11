ccbd6d54d3472c63856e79e27dece370667309b5
Louise Shen <CinemaMediaGroup@iCloud.com>
2021-01-11 12:07:22 +0800

    build(project): rebuild

--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import WebP_swiftTests
-
-var tests = [XCTestCaseEntry]()
-tests += WebP_swiftTests.allTests()
-XCTMain(tests)
---
+++ WebP.Swift/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  WebP.Swift
+//
+//  Created by Louis Shen on 2021/1/11.
+//
+
+import Foundation
+
+print("Hello, World!")
+
